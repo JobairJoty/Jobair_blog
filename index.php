d8e94a0a7b0544dcbb38334d038d680f35ad8772
JobairJoty <Jobair_joty@hotmail.com>
2018-05-04 01:45:29 +0600

    2nd workable
    it work just file but need to polish this

--- index.php
+++ index.php
@@ -27,12 +27,53 @@
 </header>
 	<section>
 		<div id="home">
-			<img src="icons/jobair_home.jpg">
+			<img src="icons/home_jobair.jpg">
 			<div id="home_name">
 				<h1>Hello I'm <br><b>Md.Jobair Ahmed</b></h1>
 				<h4>UX designer/Front End Designer,Data Scientist</h4>
 			</div>			
 		</div>
+
+		<div id="About">
+
+			<img src="icons/home_jobair.jpg">
+			<div id="About_info">
+				<h1>About Me</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			
+				<table>
+				  <tr>
+				    <td>Name : </td>
+				    <td>Md.Jobair Ahmed</td>
+				  </tr>
+				  <tr>
+				    <td>Email : </td>
+				    <td>Jobair.rajbd@gmail.com</td>
+				  </tr>
+				  <tr>
+				    <td>Phone : </td>
+				    <td>+8801773691889</td>
+				  </tr>
+				  <tr>
+				    <td>Nationality</td>
+				    <td>Bangladeshi</td>
+				  </tr>
+				</table>	
+
+			</div>	
+
+		</div>
+
+		<div id="skills">
+			
+
+		</div>
+
 	</section>
 <footer>
 	
