a0e8345ae36a4d6729190cfedfc03de58454a9ba
JobairJoty <Jobair_joty@hotmail.com>
2018-05-04 00:21:36 +0600

    first workable
    header is done and home section working is done almost.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jobair Joty</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stylesheets/header.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+</head>
+<body>
+<header>
+	<div id="nav_a">
+		<a href="#">Home</a><hr>
+		<a href="#">Blog</a><hr>
+		<a href="#">Education</a><hr>
+		<a href="#">Skill</a><hr>
+		<a href="#">About me</a><hr>
+		<a href="#">Education</a>
+	</div>
+	<div id="nav_connection">
+		<i class="fab fa-stack-overflow fa-2x"></i>
+		<i class="fab fa-linkedin fa-2x"></i>
+		<i class="fab fa-github fa-2x"></i>
+		<i class="fab fa-facebook-square fa-2x"></i>
+		<i class="fab fa-youtube fa-2x"></i>
+
+	</div>
+</header>
+	<section>
+		<div id="home">
+			<img src="icons/jobair_home.jpg">
+			<div id="home_name">
+				<h1>Hello I'm <br><b>Md.Jobair Ahmed</b></h1>
+				<h4>UX designer/Front End Designer,Data Scientist</h4>
+			</div>			
+		</div>
+	</section>
+<footer>
+	
+</footer>
+<script defer src="https://use.fontawesome.com/releases/v5.0.11/js/all.js" integrity="sha384-ImVoB8Er8knetgQakxuBS4G3RSkyD8IZVVQCAnmRJrDwqJFYUE4YOv+DbIofcO9C" crossorigin="anonymous"></script>
+</body>
+</html>
